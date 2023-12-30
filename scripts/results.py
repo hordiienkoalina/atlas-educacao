edae5f579b92b24a4e3f5e6a4fd39587012d11a1
Alina Hordiienko <alinahordiienko@gmail.com>
2023-12-30 16:03:12 +0000

    Still buggy 👺 but simplified results.py

--- scripts/results.py
+++ scripts/results.py
@@ -1,29 +1,20 @@
 import geopandas as gpd
-import geobr
 import pandas as pd
+from shapely import wkt
 
 access_data_path = "data/access_df.csv"
-common_column = "city_id"
 
 # Load access data
 access_df = pd.read_csv(access_data_path)
 
-# Download geospatial data for Brazil's municipalities
-cities_geom = geobr.read_municipality(code_muni='all', year=2010)
+# Ensure the geometry column is in a format that can be interpreted as such, i.e. WKT
+geometry = 'geometry'
+access_df[geometry] = access_df[geometry].apply(wkt.loads)
 
-# Check for invalid geometries
-# print(cities_geom.is_valid.sum())
-
-# Convert city_id to the same data type as code_muni
-# access_df['city_id'] = access_df['city_id'].astype(cities_geom['code_muni'].dtype)
-
-# Merge access data with geospatial data
-access_gdf = gpd.GeoDataFrame(access_df.merge(cities_geom, left_on='city_id', right_on='code_muni', how='inner'), geometry='geometry')
-
-# Check the number of rows before and after to see how many were dropped
-print("Before filtering: ", len(access_df))
-print("After filtering: ", len(access_gdf))
+# Convert the DataFrame to a GeoDataFrame and set the CRS at the same time
+access_gdf = gpd.GeoDataFrame(access_df, geometry=geometry, crs="EPSG:4326")
 
 # Convert the GeoDataFrame to GeoJSON
-access_gdf.to_file("access_data.geojson", driver='GeoJSON')
-print("GeoJSON file has been created successfully!")
+output_geojson_path = "access_data.geojson"
+access_gdf.to_file(output_geojson_path, driver='GeoJSON')
+print(f"GeoJSON file has been created successfully at {output_geojson_path}!")
