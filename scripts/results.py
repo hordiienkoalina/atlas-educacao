89de95b413f1ccce54c5135241a8d19b2c54770c
Alina Hordiienko <alinahordiienko@gmail.com>
2023-12-30 16:21:06 +0000

    results.py fixed (I think) 🤞🏻
    .DS_Store keeps getting pushed to git and for some reason adding it to .gitignore does not help.

--- scripts/results.py
+++ scripts/results.py
@@ -14,6 +14,11 @@ access_df[geometry] = access_df[geometry].apply(wkt.loads)
 # Convert the DataFrame to a GeoDataFrame and set the CRS at the same time
 access_gdf = gpd.GeoDataFrame(access_df, geometry=geometry, crs="EPSG:4326")
 
+# Reset the index of the GeoDataFrame
+access_gdf["row_id"] = access_gdf.index + 1
+access_gdf.reset_index(drop=True, inplace=True)
+access_gdf.set_index("row_id", inplace = True)
+
 # Convert the GeoDataFrame to GeoJSON
 output_geojson_path = "access_data.geojson"
 access_gdf.to_file(output_geojson_path, driver='GeoJSON')
