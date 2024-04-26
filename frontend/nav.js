703462033cb14cfc52b3e7fbe2cc1baa7eeb1529
Felipe Oliveira <felipe.oliveira@wiliot.com>
2024-04-26 17:06:21 -0400

    first version of navigation bar

---
+++ frontend/nav.js
@@ -0,0 +1,9 @@
+// Function to open navigation menu
+function openNav() {
+    document.getElementById("myNav").style.width = "250px"; // or 100% if you want full width
+}
+
+// Function to close navigation menu
+function closeNav() {
+    document.getElementById("myNav").style.width = "0";
+}
